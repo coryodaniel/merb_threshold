0469d7c4f140643b3ec6641db60a660b56ae802b
Cory ODaniel <github@coryodaniel.com>
2009-02-24 21:14:27 -0800

    removed a bunch of old comments

--- lib/merb_threshold/merbtasks.rb
+++ lib/merb_threshold/merbtasks.rb
@@ -30,34 +30,11 @@ end
 namespace :audit do
   desc "Print out all thresholds"
   task :thresholds => :merb_env do
+    puts "Thresholds:"
     Merb::Controller.send(:class_variable_get, "@@_threshold_map").each do |name,opts|
       limit = opts[:limit].is_a?(Array) ? 
         Merb::Threshold::Frequency.new(*opts[:limit]) : opts[:limit]
       puts " ~ #{name}: #{limit.to_s}"
     end
-    # puts "\nThresholds:\n\n"
-    # abstract_controller_classes.each do |klass|
-    #   if klass.respond_to?(:subclasses_list)
-    #     puts klass
-    #     subklasses = klass.subclasses_list.sort.map { |x| Object.full_const_get(x) }
-    #     unless subklasses.empty?
-    #       subklasses.each { |subklass| 
-    #         puts "- #{subklass}" 
-    #         unless subklass._threshold_map.empty?
-    #           subklass._threshold_map.each do |name,opts|
-    #             limit = opts[:limit].is_a?(Array) ? 
-    #               Merb::Threshold::Frequency.new(*opts[:limit]) : opts[:limit]
-    #             puts "  ~ #{name}: #{limit.to_s}"  
-    #           end
-    #         else
-    #           puts "  ~ no thresholds"
-    #         end
-    #       }
-    #     else
-    #       puts "~ no subclasses"
-    #     end
-    #     puts
-    #   end
-    # end
   end
 end
